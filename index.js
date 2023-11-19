d4246878a30cd109d0e48cdce73e5f9827cc0643
yskilictest <yskilictest@gmail.com>
2023-11-19 20:13:31 +0100

    changed2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 let exec = require('child_process').exec;
 const simpleGit = require('simple-git');
+simpleGit().clean(simpleGit.CleanOptions.FORCE);
+const git = simpleGit();
 
 
 try {
   
-  simpleGit.pull('origin', 'master', { '--no-rebase': null });
+  console.log(git.pull('origin', 'master', { '--no-rebase': null }));
 //  await git.checkout('test');
 
 } catch (error) {
